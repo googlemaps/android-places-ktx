502e67838277e0739ff4542855a65bf6ce36100e
Chris Arriola <chrisarriola@google.com>
2021-06-25 11:08:42 -0700

    Bump dependency. Remove deprecated dep.
    Change-Id: Iea8e1fe44ed09f5488dd15d4edaae46309bcd0fe

--- app/src/main/java/com/google/places/android/ktx/demo/inject/DemoModule.kt
+++ app/src/main/java/com/google/places/android/ktx/demo/inject/DemoModule.kt
@@ -20,12 +20,12 @@ import com.google.android.libraries.places.api.net.PlacesClient
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ApplicationComponent
 import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 @Module
-@InstallIn(ApplicationComponent::class)
+@InstallIn(SingletonComponent::class)
 object DemoModule {
 
     @Singleton
